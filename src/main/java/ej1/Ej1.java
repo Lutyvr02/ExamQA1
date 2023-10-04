8f25ac07f449c95ff947ec012d0becf7567d3531
Lutyvr02 <lutyvr02@gmail.com>
2023-10-04 18:36:36 -0400

    primer commit

--- src/main/java/ej1/Ej1.java
+++ src/main/java/ej1/Ej1.java
@@ -1,4 +1,25 @@
 package ej1;
 
 public class Ej1 {
+    private static final double SalarioBasico = 2000.0;
+
+    public double verificarDescuento(double descuento) throws SalarioNoValidoException{
+        if (descuento < 0){
+            throw new SalarioNoValidoException("Salario No valido");
+
+        }
+        if (descuento <= SalarioBasico){
+            return SalarioBasico;
+        } else if (descuento <= 2*SalarioBasico) {
+            return SalarioBasico - (SalarioBasico * 0.05);
+        }else {
+            return SalarioBasico - (SalarioBasico * 0.15);
+        }
+    }
+
+    public class SalarioNoValidoException extends Exception {
+        public SalarioNoValidoException(String message){
+            super(message);
+        }
+    }
 }
--- src/test/java/ej1Test/Ej1Test.java
+++ src/test/java/ej1Test/Ej1Test.java
@@ -1,4 +1,31 @@
 package ej1Test;
 
+import ej1.Ej1;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 public class Ej1Test {
+
+    private Ej1 ej1;
+
+    @BeforeAll
+    public void setEj1(){ej1 = new Ej1();}
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "2000.0, 2000.0",
+            "1000.0, 1000.0",
+            "2000.0, 2000.0",
+            "2000.0, 2000.0",
+            "2000.0, 2000.0",
+            "2000.0, 2000.0",
+
+    })
+    public void paratest(double n, double expec) throws Ej1.SalarioNoValidoException {
+        double curr = ej1.verificarDescuento(n);
+        Assertions.assertEquals(expec, curr, "Error en la prueba");
+    }
 }
